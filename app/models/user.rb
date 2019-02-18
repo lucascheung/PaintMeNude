955e88eecfaf42093b4008885c40cd5514949cfb
Lucas Cheung <lucascheungls@gmail.com>
2019-02-18 16:39:15 +0100

    drop user model

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  has_one :nudie
-  has_many :appointments
-
-  has_many :nudies_as_canvases, through: :appointments, class_name: 'Nudie', foreign_key: :nudie_id
-end
