a32d2b380d671288e48f186daf466ff2c1df5178
Lucas Cheung <lucascheungls@gmail.com>
2019-02-18 16:53:30 +0100

    delete all models

--- app/models/nudie.rb
+++
@@ -1,9 +0,0 @@
-class Nudie < ApplicationRecord
-  has_many :appointments
-  belongs_to :user
-  has_many :monkeys, through: :appointments, class_name: 'User', foreign_key: :user_id
-
-  validates :weight, presence: true
-  validates :price, presence: true
-  validates :description, presence: true
-end
--- db/migrate/20190218145115_add_user_to_nudie.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToNudie < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :nudies, :user
-  end
-end
