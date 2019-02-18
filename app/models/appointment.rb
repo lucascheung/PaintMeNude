d6a0f7ed37322ffc9aef6c2397617286925a04fd
Kristian Soelling <kristiansoelling@gmail.com>
2019-02-18 17:48:07 +0100

    Apointment validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :nudie
+  validates :appointment_date, presence: true
+  validates :location, presence: true
+  validates :user, presence: true
+  validates :nudie, presence: true
 end
