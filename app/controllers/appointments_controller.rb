24e9ef92f2016cabca5918c2f18e775f9cc2b9f2
Kristian Soelling <kristiansoelling@gmail.com>
2019-02-20 14:21:24 +0100

    Appointment resource change

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -13,7 +13,7 @@ class AppointmentsController < ApplicationController
   def create
     @appointment = Appointment.find(set_appointment)
     @appointment.user_id = current_user.id
-    @appointment.nudie_id = @nudie.id
+    @appointment.nudy_id = @nudie.id
     @appointment.save
   end
 
