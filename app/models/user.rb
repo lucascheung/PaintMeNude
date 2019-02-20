9af519a47a0115967a5a87b187ab573a8b2897eb
Lucas Cheung <lucascheungls@gmail.com>
2019-02-20 17:00:54 +0100

    add background and map

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
   mount_uploader :photo, PhotoUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
