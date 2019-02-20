e5f6b1e78f149f976c3c2a6115e673d76c55bf23
Kristian Soelling <kristiansoelling@gmail.com>
2019-02-20 13:49:50 +0100

    Added application booking page, more to come

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :nudies, only: [:new, :create, :index, :destroy, :show]
   resources :users, only: [:new, :create, :index, :destroy, :show]
+  resources :appointments, only: [:new, :create, :index, :destroy, :show]
   get 'design', to: 'pages#design'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
